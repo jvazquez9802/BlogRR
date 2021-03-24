d085a0dfabf48ed6b3ff447c3e28f3bd5ab418a3
Juan_Vazquez <jvazquez2098@gmail.com>
2021-03-23 23:38:56 -0600

    Learning rails

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,37 @@
+class ArticlesController < ApplicationController
+    
+    before_action :find_article, except: [:new, :create, :index]
+    before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy ]
+
+    def index
+        @articles = Article.all
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.create(title: params[:article][:title], content: params[:article][:content])
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+        @article.update(title: params[:article][:title], content: params[:article][:content])
+        redirect_to @article
+    end
+
+    def destroy
+        @article.destroy
+        redirect_to root_path
+    end
+
+    def find_article
+        @article = Article.find(params[:id])
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  get "welcome", to: "home#index"
+
+  root to: "home#index"
+
+  get "articles", to:"articles#index"
+  get "articles/new", to: "articles#new", as: :new_articles
+  get "articles/:id", to: "articles#show"
+  get "articles/:id/edit", to: "articles#edit"
+
+  patch "articles/:id", to: "articles#update", as: :article
+  post "articles", to: "articles#create"
+  delete "articles/:id", to: "articles#destroy"
+
 end
